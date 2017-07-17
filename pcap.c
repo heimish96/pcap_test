5c1b1221b7c29b52d0125070fc7750ca1cbb9edf
heimish96 <heimish.w@gmail.com>
2017-07-17 13:37:03 +0900

    Modify Error

--- pcap.c
+++ pcap.c
@@ -7,11 +7,12 @@ pcap_t *handle;			/* Session handle */
 char *dev;			/* The device to sniff on */
 char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
 struct bpf_program fp;		/* The compiled filter */
-char filter_exp[] = "port 23";	/* The filter expression */
+char filter_exp[] = "port 80";	/* The filter expression */
 bpf_u_int32 mask;		/* Our netmask */
 bpf_u_int32 net;		/* Our IP */
 struct pcap_pkthdr header;	/* The header that pcap gives us */
 const u_char *packet;		/* The actual packet */
+int pcap_mod=0;
 
 /* Define the device */
 dev = pcap_lookupdev(errbuf);
@@ -39,42 +40,45 @@ if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
 if (pcap_setfilter(handle, &fp) == -1) {
 	fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
 	return(2);
-}
+
+	}
 
 
-while(1){
+while(1)
+{
 	
-	int module=0;
 	/* Grab a packet */
-	module=pcap_next_ex(handle, &header, &packet);
+	pcap_mod=pcap_next_ex(handle, &header, &packet);
 
 	/* Print its length */
 	printf("Jacked a packet with length of [%d]\n", header.len);
-	
-	if(module==0)
+	if(pcap_mod == 0)
 	{	
 		continue;	
 	}
 
 	printf("This is Ethernet Header information space \n");
-	printf("Destination Mac Address: %02x %02x %02x %02x %02x %02x \n",packet[0],packet[1],packet[2],packet[3],packet[4],packet[5]);
-	printf("Source Mac Address: %02x %02x %02x %02x %02x %02x \n",packet[6],packet[7],packet[8],packet[9],packet[10],packet[11]);
+	printf("Destination Mac Address: %02x:%02x:%02x:%02x:%02x:%02x \n",packet[0],packet[1],packet[2],packet[3],packet[4],packet[5]);
+	printf("Source Mac Address: %02x:%02x:%02x:%02x:%02x:%02x \n",packet[6],packet[7],packet[8],packet[9],packet[10],packet[11]);
 
 	if(packet[12]==0x08 && packet[13]==0x00)
 	{
-		printf("This is [%d] IP Header information space \n");
+		printf("This is IP Header information space \n");
 		printf("Source IP Address: %d.%d.%d.%d \n",packet[26], packet[27], packet[28], packet[29]);
 		printf("Destination IP Address: %d.%d.%d.%d \n", packet[30], packet[31], packet[32], packet[33]);
 	}
 	
-	printf("This is TCP Header information space \n",i);
-	printf("Source Port: %d", ((packet[34]*256)+packet[35]);
-	printf("Destination Port: %d", ((packet[36]*256)+packet[37]);
+	printf("This is TCP Header information space \n");
+	printf("Source Port: %d", packet[34]*256+packet[35]);
+	printf("Destination Port: %d", packet[36]*256+packet[37]);
 
 	
+	
 }
-/* And close the session */
-pcap_close(handle);
-return(0);
+
+	/* And close the session */
+	pcap_close(handle);
+	return(0);
+	
 }
   
